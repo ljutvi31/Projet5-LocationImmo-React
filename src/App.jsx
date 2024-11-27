3ebec647ce3c1baef2be98090fbf3c30024a16cb
Harry Ljutvi <ljutvi.h@gmail.com>
2024-11-27 15:18:35 +0100

    Routes created + Pages

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
+import { Routes, Route } from 'react-router-dom'
+import Home from './Pages/Home'
+import About from './Pages/About'
+import Housing from './Pages/Housing'
+import Error from './Pages/Error'
+
 function App() {
   return (
-    <>
-      <h1>Kasa</h1>
-    </>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/about" element={<About />} />
+      <Route path="/housing/:id" element={<Housing />} />
+      <Route path="*" element={<Error />} />
+    </Routes>
   )
 }
 
---
+++ src/Pages/Housing.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Housing() {
+    return (
+        <div>
+            <h1>Logement</h1>
+        </div>
+    )
+}
+
+export default Housing
