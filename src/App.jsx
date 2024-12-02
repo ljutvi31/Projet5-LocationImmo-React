59b330e22c0cb891883b0add26e3ee2aadae4a9c
Harry Ljutvi <ljutvi.h@gmail.com>
2024-12-02 14:09:43 +0100

    files adjustment

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import Router from './components/Router'
-import MainLayout from './components/layouts/MainLayout'
+import MainLayout from './components/Layouts/MainLayout'
 
 function App() {
   return (
