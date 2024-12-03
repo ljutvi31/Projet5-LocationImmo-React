86fb1e5d1d98c3d37412d01479c7d763a336e0c2
Harry Ljutvi <ljutvi.h@gmail.com>
2024-12-03 14:13:49 +0100

    home page: cards adjustment, banner & logo

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,9 @@
 import React from 'react'
+import banniere from './images/banniere.jpg'  // Import de l'image
 
 function Banner() {
     return (
-        <div className="banner">
+        <div className="banner" style={{backgroundImage: `url(${banniere})`}}>
             <h1>Chez vous, partout et ailleurs</h1>
         </div>
     )
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import "../../styles/components/Header.scss";
+import logo from '../images/logo.png'  // Chemin depuis Header vers images
+import '../../styles/components/Header.scss'
 
 function Header() {
     return (
         <header>
+            <Link to="/">
+                <img src={logo} alt="Logo Kasa" className="logo" />
+            </Link>
             <nav>
                 <Link to="/">Accueil</Link>
                 <Link to="/about">À Propos</Link>
