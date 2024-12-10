2ed8f4c65df4c822783791a4f8e18b26d46e16d0
Harry Ljutvi <ljutvi.h@gmail.com>
2024-12-10 11:50:49 +0100

    style for housing.scss

--- src/pages/housing.jsx
+++ src/pages/housing.jsx
@@ -1,21 +1,78 @@
 import React from 'react';
-import { useParams, Navigate } from 'react-router-dom';
+import { useParams } from 'react-router-dom';
 import Slideshow from '../components/Slideshow';
-import logements from '../Data/logements.json';
+import Error from '../pages/error';
+import Collapse from '../components/Collapse';
+import logements from '../data/logements.json';
+import '../styles/components/housing.scss';
 
 function Housing() {
     const { id } = useParams();
     const housing = logements.find(logement => logement.id === id);
 
-    // Redirection vers la page d'erreur si le logement n'est pas trouvé
     if (!housing) {
-        return <Error/>; //charge le composant Error 
+        return <Error />;
     }
 
     return (
         <div className="housing">
+            {/* Section carrousel */}
             <Slideshow pictures={housing.pictures} />
-            {/* Ici viendra le reste des informations du logement */}
+            
+            <div className="housing__content">
+                {/* Section principale avec titre, localisation, tags et hôte */}
+                <div className="housing__header">
+                    {/* Partie gauche : info logement */}
+                    <div className="housing__info">
+                        <h1>{housing.title}</h1>
+                        <p className="housing__location">{housing.location}</p>
+                        {/* Section des tags */}
+                        <div className="housing__tags">
+                            {housing.tags.map((tag, index) => (
+                                <span key={index} className="tag">{tag}</span>
+                            ))}
+                        </div>
+                    </div>
+
+                    {/* Partie droite : info hôte et notation */}
+                    <div className="housing__host">
+                        <div className="host">
+                            <p className="host__name">{housing.host.name}</p>
+                            <img src={housing.host.picture} alt={`Portrait de ${housing.host.name}`} className="host__picture" />
+                        </div>
+                        {/* Système de notation avec étoiles */}
+                        <div className="rating">
+                            {[...Array(5)].map((_, index) => (
+                                <span 
+                                    key={index} 
+                                    className={`star ${index < housing.rating ? 'star--active' : ''}`}
+                                >
+                                    ★
+                                </span>
+                            ))}
+                        </div>
+                    </div>
+                </div>
+
+                {/* Section Collapse description et équipements */}
+                <div className="housing__details">
+                    <div className="housing__collapse">
+                        <Collapse title="Description" content={housing.description} />
+                    </div>
+                    <div className="housing__collapse">
+                        <Collapse 
+                            title="Équipements" 
+                            content={
+                                <ul>
+                                    {housing.equipments.map((item, index) => (
+                                        <li key={index}>{item}</li>
+                                    ))}
+                                </ul>
+                            }
+                        />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
